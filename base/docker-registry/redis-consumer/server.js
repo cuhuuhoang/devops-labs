21752f76fee44185ba2c456162c011424e283e58
hoangch <hoangch@coccoc.com>
2022-05-22 12:34:39 +0700

    add uweb

--- base/docker-registry/redis-consumer/server.js
+++ base/docker-registry/redis-consumer/server.js
@@ -1,4 +1,14 @@
 const express = require('express')
+const redis = require("redis");
+
+const client = redis.createClient("redis://10.106.130.41:6379")
+async function start() {
+await client.connect();
+console.log('connected');
+client.quit();
+};
+start();
+
 const app = express()
 const port = 3000
 
