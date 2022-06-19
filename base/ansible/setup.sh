e789f7c0b268313ec0640b210b6e977847d94961
hoangch <hoangch@coccoc.com>
2022-06-19 11:18:01 +0700

    add ansible

---
+++ base/ansible/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "install ansible"
+sudo yum install -y epel-release
+sudo yum install -y ansible
+
+sudo cp hosts /etc/ansible/hosts
+
+#ansible all -m ping
+#ansible all -m command -a "uptime"
+#ansible all -m setup -a 'filter=ansible_distribution'
+#ansible all -m setup -a 'filter=ansible_default_ipv4'
+
+ansible-playbook -i /etc/ansible/hosts playbook-wordpress.yml
--- base/ssh-key/setup.sh
+++ base/ssh-key/setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-mv id_rsa_nearlytech ~/.ssh/id_rsa
+cp id_rsa_nearlytech ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-mv id_rsa_nearlytech.pub ~/.ssh/authorized_keys
+cp id_rsa_nearlytech.pub ~/.ssh/authorized_keys
 chmod 644 ~/.ssh/authorized_keys
 
 sudo systemctl restart sshd
+
+for i in {1..3}; do
+	ssh -o StrictHostKeyChecking=no "a$i.vt" uname -n
+done
