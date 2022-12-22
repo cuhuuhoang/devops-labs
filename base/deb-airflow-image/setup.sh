23d34425669114622100973764adcecb9c6d71a1
hoangch <hoangch@coccoc.com>
2022-12-22 11:38:06 +0700

    working airflow

--- base/deb-airflow-image/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo ctr -n=k8s.io images rm docker.io/library/airflow:1.0.0
-sudo ctr -n=k8s.io images import airflow.tar
--- base/deb-airflow-init/setup.sh
+++ base/deb-airflow-init/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+kubectl -n airflow apply -f airflow-init-deployment.yaml
+sleep 60s
+kubectl -n airflow exec deploy/airflow-init -- sh -c "chown -R 50000:0 /xx"
+sleep 5s
 kubectl -n airflow delete -f airflow-init-deployment.yaml
-#kubectl -n airflow apply -f airflow-init-deployment.yaml
